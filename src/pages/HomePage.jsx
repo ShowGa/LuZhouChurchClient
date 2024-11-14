fd0f4c218197b6e66cceb034862d9d3992df124e
ShowGa <showgacareer@gmail.com>
2024-11-14 14:26:39 +0800

    <Progress> 1. Hero sec swiper structure 2. On the road to complete hero description

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,59 @@
 import React from "react";
-import { img8 } from "../assets";
+
+import { heroImages } from "./consts/consts";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+import SwiperCore from "swiper";
+import { Navigation, Pagination, EffectFade, Autoplay } from "swiper/modules";
+
+import "./css/pagesCSS.css";
 
 const HomePage = () => {
+  SwiperCore.use([Navigation, Pagination, EffectFade, Autoplay]);
+
   return (
     <main>
-      <img src={img8} alt="" />
+      {/* Hero Section */}
+      <section>
+        <div className="c-hero_section_container h-[50rem] max-md:h-[35rem]">
+          {/* swiper container */}
+          <div className="c-hero_swiper-container">
+            <Swiper
+              modules={[Navigation, Pagination, EffectFade, Autoplay]}
+              autoplay={{
+                delay: 4000,
+                disableOnInteraction: false,
+              }}
+              loop={true}
+              effect="fade"
+            >
+              {heroImages.map((image) => (
+                <SwiperSlide key={image.id}>
+                  <img src={image.image} alt="" />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
+          {/* content container */}
+          <div className="c-hero_content_container">
+            <div>
+              <h1></h1>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
 
 export default HomePage;
+
+// swiper suit the container height
+
+// swiper should be absolutely positioned
+
+// create the Biblical scriptures cards component later
+
+// create the  cards component later
---
+++ src/pages/consts/consts.js
@@ -0,0 +1,7 @@
+import { img8, img9, img10 } from "../../assets";
+
+export const heroImages = [
+  { id: 1, image: img8 },
+  { id: 2, image: img9 },
+  { id: 3, image: img10 },
+];
