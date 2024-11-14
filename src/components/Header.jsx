a3afeaebbd9d1388811eb76c5b5ff8f7c53951a2
ShowGa <showgacareer@gmail.com>
2024-11-14 10:43:39 +0800

    <Progress, Modify> 1. Progress : create sidebar 2. Modify : header css RWD

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,31 +14,47 @@ import "./css/componentCSS.css";
 const Header = () => {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
 
+  /* ============================ */
+  /*        Helper function       */
+  /* ============================ */
+
   return (
     <header className="c-header">
       {/* main nav bar */}
       <div className="c-nav_container">
         {/* left nav bar */}
         <div className="c-header_left">
-          <Nav navData={navListItemsLeft} />
+          <div className="max-md:hidden">
+            <Nav navData={navListItemsLeft} />
+          </div>
         </div>
 
         {/* logo container */}
-        <div className="c-header_logo_container">
+        <div className="c-header_logo_container left-1/2 transform -translate-x-1/2 max-md:left-4 max-md:transform-none max-md:translate-x-0">
           <div>
             <img className="w-16 h-16 rounded-full" src={img1} alt="" />
           </div>
-          <div className="text-yellow-400">
+          <div className="text-yellow-500">
             <p className="text-3xl font-bold">蘆洲禮拜堂</p>
             <p className="text-xs">LuZhou Church</p>
           </div>
         </div>
 
         {/* right nav bar */}
-        <div className="c-header_right">
-          <Nav navData={navListItemsRight} />
+        <div className={`c-header_right justify-between max-md:justify-end`}>
+          <div className="max-md:hidden">
+            <Nav navData={navListItemsRight} />
+          </div>
 
           <ul className="c-header_right_function_container">
+            {true ? (
+              <li>
+                <img className="w-10 h-10 rounded-full" src={img1} alt="img1" />
+              </li>
+            ) : (
+              <li>Login</li>
+            )}
+
             <li className="flex items-center justify-center text-yellow-500 md:hidden">
               <button onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}>
                 {isMobileNavOpen ? (
@@ -48,14 +64,6 @@ const Header = () => {
                 )}
               </button>
             </li>
-
-            {true ? (
-              <li>
-                <img className="w-10 h-10 rounded-full" src={img1} alt="img1" />
-              </li>
-            ) : (
-              <li>Login</li>
-            )}
           </ul>
         </div>
       </div>
